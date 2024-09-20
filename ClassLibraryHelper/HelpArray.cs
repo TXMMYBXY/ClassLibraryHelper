9c7c08db90e09839f45587e4fd8749675ca060b5
TXOMMYBXY <altusha_03@bk.ru>
2024-09-20 23:59:23 +0700

    Добавьте файлы проекта.

---
+++ ClassLibraryHelper/HelpArray.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryHelper
+{
+    public class HelpArray
+    {
+        public static Array MakeArray(Type type, int length)
+        {
+            var rnd = new Random();
+
+            var array = Array.CreateInstance(type, length);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (type == typeof(char)) array.SetValue((char)rnd.Next(0, 50), i);
+                else array.SetValue(rnd.Next(0, 50), i);
+            }
+            return array;
+        }
+
+        public static Array SortArray(Array array, IComparer comparer)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (comparer.Compare(array.GetValue(i), array.GetValue(j)) < 0)
+                    {
+                        var temp = array.GetValue(j);
+                        array.SetValue(array.GetValue(i), j);
+                        array.SetValue(temp, i);
+                    }
+                }
+            }
+            return array;
+        }
+
+        public static void WriteArray(Array array)
+        {
+            Console.WriteLine("Массив:");
+            foreach (var element in array)
+                Console.WriteLine(element.ToString());
+        }
+    }
+
+    public class SortToUp : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            var obj1 = (IComparable)x;
+            var obj2 = (IComparable)y;
+            return obj1.CompareTo(obj2);
+        }
+    }
+
+    public class SortToDown : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            var obj1 = (IComparable)x;
+            var obj2 = (IComparable)y;
+            return obj2.CompareTo(obj1);
+        }
+    }
+}
---
+++ ClassLibraryHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ClassLibraryHelper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassLibraryHelper")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("500d5990-0279-4483-9d5a-3bca63d2c5a8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
